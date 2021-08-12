38aae17de467a05879bc864abf01af35497e26a2
andresjaradeeploy <85570903+andresjaradeeploy@users.noreply.github.com>
2021-08-12 11:30:43 -0500

    sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,13 +1,13 @@
 
-insert into cuenta_fb_developer(id_cuenta,access_token,id_aplicacion,secret_key) values(1000001,"EAAp4gTbHgEoBAAONi8R4894QdXyjnNvax1ZAp5XftALhyS2HfuyCxUNQIFgTSZAuix0myV3qXG9a0ovOuEzSH1gnF9vfzzu9Y5U5C6aFAIZCuxTUf49l1oxaH92Pxno1nYlnVqspHP2eZA84AZCvkMkRFMTj80JPIsbeXs42DXXxVW7QynuOT","2947246132265034","16d7d0bc793f0f565653b29b30c32703");
+insert into cuenta_fb_developer(id_cuenta,access_token,id_aplicacion,secret_key) values(1000001,'EAAp4gTbHgEoBAAONi8R4894QdXyjnNvax1ZAp5XftALhyS2HfuyCxUNQIFgTSZAuix0myV3qXG9a0ovOuEzSH1gnF9vfzzu9Y5U5C6aFAIZCuxTUf49l1oxaH92Pxno1nYlnVqspHP2eZA84AZCvkMkRFMTj80JPIsbeXs42DXXxVW7QynuOT','2947246132265034','16d7d0bc793f0f565653b29b30c32703');
 
 
 
 insert into rol values (1,'ROLE_USER');
 
-insert into estado (id_estado,nombre_estado) values (1,"Arriba");
+insert into estado (id_estado,nombre_estado) values (1,'Arriba');
 
-insert into estado (id_estado,nombre_estado) values (2,"Caido");
+insert into estado (id_estado,nombre_estado) values (2,'Caido');
 
 insert into usuario (cargo,email,nombre) values ('Administrador','johan.jara@deeploy.co','Johan Andrés Jara Ardila')
 
