27a3ab6df669f6f0cc481c3543abe3d6b10e34b9
andresjaradeeploy <85570903+andresjaradeeploy@users.noreply.github.com>
2022-01-19 11:12:40 -0500

    Update PostRestController.java

--- src/main/java/com/monitor/bankendmonitoreoLinks/controller/PostRestController.java
+++ src/main/java/com/monitor/bankendmonitoreoLinks/controller/PostRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,21 @@ public class PostRestController {
 	public Post save(@RequestBody Post post) {
 		
 		return iPostService.save(post);
+	}
+	
+	@PutMapping("/update")
+	public Post update(@RequestBody Post post) {
+		
+		Post postNew= new Post();
+		postNew.setPost_reactions_like_total(post.getPost_reactions_like_total());
+		postNew.setPost_reactions_love_total(post.getPost_reactions_love_total());
+		postNew.setPost_reactions_wow_total(post.getPost_reactions_wow_total());
+		postNew.setPost_negative_feedback(post.getPost_negative_feedback());
+		postNew.setPost_engaged_users(post.getPost_engaged_users());
+		postNew.setPost_engaged_fan(post.getPost_engaged_fan());
+		postNew.setPost_clicks_unique(post.getPost_clicks_unique());
+		postNew.setPost_clicks(post.getPost_clicks());
+		return iPostService.save(postNew);
 	}
 	
 }
